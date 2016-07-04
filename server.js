5fd60e4bfb7fd2050866c4480000894e2c7c12ab
praveen <praveen.p@krds.fr>
2016-07-04 15:41:47 +0530

    port changed

--- server.js
+++ server.js
@@ -73,10 +73,14 @@ app.use(function(err, req, res, next) {
 });
 
 //Port Config
-const server      = require('http').Server(app);
+/*const server      = require('http').Server(app);
 const serverIp    = process.env.APP_IP || '0.0.0.0';
 const serverPort  = process.env.APP_PORT || 20310;
 
-server.listen(serverPort, serverIp);
+server.listen(serverPort, serverIp);*/
+
+app.listen(process.env.PORT || 5000, function(){
+  
+});
 
 module.exports = app;
